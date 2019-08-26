2aed5afd94a4b1cb49dc45762291a973b4b9c3cb
gr0ss1428 <gr0ss1428@gmail.com>
2019-08-26 20:26:34 +0300

    Release 0.0.0.0.0.0.1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using arrayLib;
+using BiggestNumber;
+using ClassLibrary1;
+using MathArray;
+namespace ConsoleRelease2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArraInt array = new ArraInt(10);
+
+            Console.WriteLine(array.GetSum());
+
+            Console.WriteLine(Massiv.GetSumOdd(array.GetArray()));
+
+            FindClass instance = new FindClass();
+
+            Console.WriteLine(instance.FindMax());
+
+
+            Console.ReadKey();
+        }
+    }
+}
